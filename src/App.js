576eebb00361c8abd728701e7047f649ec6ca581
mustkeem <mo.mustkem@gmail.com>
2020-06-01 10:47:06 +0530

    a comment about redux and context api

--- src/App.js
+++ src/App.js
@@ -41,3 +41,25 @@ function App() {
 }
 
 export default App;
+
+/*
+Why should we choose redux or react context api?
+
+We should not choose one over the other because it depends on use cases.
+
+Like:
+
+For low-frequency updates like locale, theme changes, 
+etc. the React Context is perfectly fine. 
+
+But with a more complex state which has high-frequency updates, 
+the React Context won't be a good solution. Because, the React 
+Context will trigger a re-render on each update, and optimizing it manually 
+can be really tough. In this scenario Redux is much easier to implement.
+
+Context doesn't give anything like the Redux DevTools where we can see our centralized state data.
+
+Redux provide the ability to handle API request in Action creators easily.
+
+So we can user redux and context api both in app. Because each one have some advantages over the other.
+*/
